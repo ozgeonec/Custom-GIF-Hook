af9cfd2fe358670f245c16531234921ea1fca0fa
ozgeonec <ozgenuronec@gmail.com>
2021-02-20 22:45:59 +0300

    custom hook project

---
+++ src/components/Tag_V2.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import useGif from '../useGif';
+
+const Tag = () => {
+    const [tag, setTag] = useState('dogs');
+    const {gif, fetchGif} = useGif(tag);
+
+    return (
+        <div className={"container"}>
+            <h1>Random {tag} Gif</h1>
+            <img width="500" src={gif} alt="random gif"/>
+            <input value={tag} onChange={(e) => setTag(e.target.value)}/>
+            <button onClick={() => fetchGif(tag)}>Click For New</button>
+        </div>
+    );
+}
+export default Tag;
